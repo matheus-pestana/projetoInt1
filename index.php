9f9f7bd20a99fa23d4596bf7dc0ac167a63eea69
Funça~ <124839957+Faelfaria@users.noreply.github.com>
2025-03-31 22:28:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <title>Início</title>
+
+</head>
+
+<body>
+    <div id="container">
+        <div id="sidebar">
+            <div class="user">
+                <img class="userImg" src="./assets/img/user.png" alt="usuário">
+                <p class="name">Nome Teste</p>
+                <p class="email">emailteste@gmail.com</p>
+            </div>
+            <div class="menu">
+                <a href=""><img src="./assets/img/lapis.png" alt="">Perfil</a>
+                <a href=""><img src="./assets/img/casa.png" alt="">Página Inicial</a>
+                <a href=""><img src="./assets/img/grafico.png" alt="">Gráficos</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
