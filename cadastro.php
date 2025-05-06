bcf783194ba39c353786a926a1962f96f74536e1
matheus0pestana@gmail.com <matheus0pestana@gmail.com>
2025-05-06 14:53:02 -0300

    half frontend

--- cadastro.php
+++ cadastro.php
@@ -1,40 +1,36 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cadastro</title>
-    <link rel="stylesheet" href="./assets/css/cadastro.css">
+    <link rel="stylesheet" href="./assets/css/login.css" />
 </head>
+
 <body>
-    <div class="container">
-        <div class="card">
-            <h2>Registrar-se</h2>
-            <form>
-                <div class="form-group">
-                    <label for="nome_completo">Nome Completo:</label>
-                    <input type="text" id="nome_completo" name="nome_completo">
-                </div>
-                <div class="form-group">
-                    <label for="nome_usuario">Nome de Usuário:</label>
-                    <input type="text" id="nome_usuario" name="nome_usuario">
-                </div>
-                <div class="form-group">
-                    <label for="email">Endereço de Email:</label>
-                    <input type="email" id="email" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="senha">Senha:</label>
-                    <input type="password" id="senha" name="senha">
-                </div>
-                <div class="form-group">
-                    <label for="confirmar_senha">Confirmar Senha:</label>
-                    <input type="password" id="confirmar_senha" name="confirmar_senha">
-                </div>
-                <button type="submit" class="register-button">Registrar</button>
-            </form>
-            <p class="login-link">Eu tenho uma conta. <a href="#">Entrar</a></p>
-        </div>
+    <div class="login-container">
+        <h2><strong>Registrar-se</strong></h2>
+        <form class="form">
+            <label for="email">Nome completo:</label><br />
+            <input type="email" id="email" name="email" required /><br />
+
+            <label for="senha">Nome de Usuário:</label><br />
+            <input type="password" id="senha" name="senha" required /><br />
+
+            <label for="senha">Endereço de Email:</label><br />
+            <input type="password" id="senha" name="senha" required /><br />
+
+            <label for="senha">Senha:</label><br />
+            <input type="password" id="senha" name="senha" required /><br />
+
+            <label for="senha">Confirmar Senha:</label><br />
+            <input type="password" id="senha" name="senha" required /><br />
+
+            <button type="submit">Entrar</button>
+        </form>
+        <p>Eu tenho uma conta! <a href="index.php">Entrar</a></p>
     </div>
 </body>
+
 </html>
---
+++ home.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quem Somos</title>
+    <link rel="stylesheet" href="./assets/css/home.css">
+</head>
+
+<body>
+    <div class="container">
+        <aside class="sidebar">
+            <div class="profile">
+                <div class="avatar"></div>
+                <h3>VINÍCIUS CARDOSO</h3>
+                <p>emailimaginario@gmail.com</p>
+            </div>
+            <nav class="menu">
+                <a href="profile.php" class="menu-item">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="./assets/icons/lapis.svg" />
+                    </svg>
+                    Perfil
+                </a>
+                <a href="#" class="menu-item active">
+                    <img src="./assets/icons/casa.png" alt="Página Inicial">
+                    Página Inicial
+                </a>
+                <a href="graphs.php" class="menu-item">
+                    <img src="./assets/icons/grafico.png" alt="Gráficos">
+                    Gráficos
+                </a>
+                <a href="logout.php" class="menu-item sair">
+                    <img src="./assets/icons/sair.png" alt="Sair">
+                    Sair
+                </a>
+            </nav>
+        </aside>
+
+        <main class="main-content">
+            <div class="title">
+                <h1>QUEM SOMOS?</h1>
+            </div>
+            <div class="profiles">
+                <section class="profile-card">
+                    <img src="./assets/img/matheus.png" alt="Matheus Arcangelo" class="foto">
+                    <div class="info">
+                        <h2>MATHEUS ARCANGELO PESTANA</h2>
+                        <p>Técnico em Desenvolvimento de Sistemas e cursando Análise e Desenvolvimento de Sistemas, ambos pelo SENAI Félix Guisard. Sou apaixonado pela tecnologia e suas inovações. Sempre busco dedicar meu máximo aos trabalhos que me são atribuídos e em manter a integridade e andamento da equipe em que estiver.</p>
+                        <div class="links">
+                            <a href="https://www.linkedin.com/in/matheus-arcangelo/">
+                                <img src="./assets/icons/linkedin.png" alt="LinkedIn">
+                            </a>
+                            <a href="https://github.com/matheus-pestana">
+                                <img src="./assets/icons/github.png" alt="GitHub">
+                            </a>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="profile-card">
+                    <img src="./assets/img/vinicius.png" alt="Vinícius Cardoso" class="foto">
+                    <div class="info">
+                        <h2>VINÍCIUS CARDOSO DE PAULA</h2>
+                        <p>Técnico em Multimídia e cursando Análise e Desenvolvimento de Sistemas pela faculdade SENAI Félix Guisard. Dedico meu tempo ao uso da minha criatividade e ao aprendizado constante, cada desafio me anima mais a construir um caminho profissional de sucesso.</p>
+                        <div class="links">
+                            <a href="https://www.linkedin.com/in/vinícius-cardoso-de-paula/">
+                                <img src="./assets/icons/linkedin.png" alt="LinkedIn">
+                            </a>
+                            <a href="https://www.instagram.com/vini7.c/">
+                                <img src="./assets/icons/instagram.png" alt="Instagram">
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <footer class="footer">
+                <div class="footer-icons">
+                    <img src="./assets/icons/instagram_footer.png" alt="Instagram">
+                    <img src="./assets/icons/linkedin_footer.png" alt="LinkedIn">
+                    <img src="./assets/icons/facebook_footer.png    " alt="Facebook">
+                </div>
+                <p>2025 SENAI. Todos os direitos reservados</p>
+            </footer>
+        </main>
+    </div>
+
+</body>
+
+</html>
